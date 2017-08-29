0c195a7b0324262562ea0d31439856f899defb1c
Chiawen_Wang <wendy_wangchiawen@outlook.com>
2017-08-29 15:20:27 +0800

    Fix bugs

--- markdown/build.js
+++ markdown/build.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-fs.readdir('./markdown', function(err,files){
+fs.readdir('E:/Tensent_BAT/WebDevelopBlog/markdown', function(err,files){
+	console.log(files);
 	for(var i=0; i<files.length; i++){
-	var p = path.join('./markdonw',files[i]);
+	var p = path.join('E:/Tensent_BAT/WebDevelopBlog/markdown',files[i]);
 	var markdown = fs.readFileSync(p).toString();
-	var template = fs.readFileSync('./template.html').toString();
+	var template = fs.readFileSync('E:/Tensent_BAT/WebDevelopBlog/template.html').toString();
 	var result = template.replace('%content%',markdown);
 	fs.writeFileSync(files[i]+'.html',result);
 	}
